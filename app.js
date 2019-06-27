1441cb9ab5cd26361e1d3f2fd7206b5c21940816
Jeancy Mungedi <jeancymungedi@gmail.com>
2019-06-27 16:08:11 +0200

    Last change

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+app.use(session[{
+    secret: "Our Little Secret",
+    resave: false,
+    saveUninitialized : false
+
+}]);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -21,58 +33,35 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res){
-    res.render("home")
+    res.render("home");
 });
 
 app.get("/login", function(req, res){
-    res.render("login")
+    res.render("login");
 });
+
 app.post("/login", function(req, res){
-
-    const email = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({ email: email}, function(err, foundUser){
-
-        if(err){
-            console.log(err);
-        } else {
-            if(foundUser){
-                if(foundUser.password === password){
-                    console.log(foundUser.password); 
-                    res.render("secrets");
-                }else {
-                    res.send("The User and Password don't Match");
-                }
-            }
-        }
-    })
-})
+  
+});
 
 app.get("/register", function(req, res){
     res.render("register")
 });
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        } else {
-            res.send(err);
-        }
-    });
+app.post("/register", function(req, res){
+    
 });
+    
 
 
 
